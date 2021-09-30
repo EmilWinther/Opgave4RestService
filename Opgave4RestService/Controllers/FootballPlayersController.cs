f927dac3b3e5ba814548fb8815bcb75e618a452d
Ekmul <emil377g@edu.easj.dk>
2021-09-30 11:54:47 +0200

    first commit

---
+++ Opgave4RestService/Controllers/FootballPlayersController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FootBallLib;
+using Opgave4RestService.Managers;
+
+namespace Opgave4RestService.Controllers
+{
+    public class FootballPlayersController : Controller
+    {
+        private readonly FootballPlayerManager _footballPlayerManager = new FootballPlayerManager();
+        // GET: FootballPlayersController
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _footballPlayerManager.GetAll();
+        }
+
+        // GET: FootballPlayersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FootballPlayersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FootballPlayersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FootballPlayersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FootballPlayersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FootballPlayersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FootballPlayersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Opgave4RestService/Managers/FootballPlayerManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FootBallLib;
+
+namespace Opgave4RestService.Managers
+{
+    public class FootballPlayerManager
+    {
+        private static int _nextId = 1;
+
+        private static readonly List<FootballPlayer> Data = new List<FootballPlayer>
+        {
+            new FootballPlayer(1, "e", 2323, 2323)
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Data);
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Data.Find(footballPlayer => footballPlayer.Id == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newFP)
+        {
+            newFP.Id = _nextId++;
+            Data.Add(newFP);
+            return newFP;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer FP = Data.Find(footballPlayer1 => footballPlayer1.Id == id);
+            if (FP == null) return null;
+            Data.Remove(FP);
+            return FP;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer FP1 = Data.Find(footballPlayer1 => footballPlayer1.Id == id);
+            if (FP1 == null) return null;
+            FP1.Name = updates.Name;
+            FP1.Price = updates.Price;
+            return FP1;
+        }
+    }
+}
